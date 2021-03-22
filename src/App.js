4b149ea56112e8d187deb32fadd808e7388ff10c
Simon Catalin <simoncatalin@Simons-MacBook-Air.local>
2021-03-22 16:48:35 +0200

    implamented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.util';
+import { auth, createUserProfileDocument } from './firebase/firebase.util';
 
 class App extends React.Component {
   constructor() {
@@ -21,8 +21,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          });
+          console.log(this.state);
+        }); 
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -14,13 +14,13 @@ class SignIn extends React.Component {
         this.state = {
             email: '',
             password: ''
-        }
+        };
     }
 
     handleSubmit = event => {
         event.preventDefault();
 
-        this.setState({email: '', password: ''});
+        this.setState({ email: '', password: '' });
     };
 
     handleChange = event => {
@@ -35,20 +35,20 @@ class SignIn extends React.Component {
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
 
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmit}> 
                     <FormInput 
                         name='email'
                         type='email' 
-                        value={this.state.email} 
                         handleChange={this.handleChange}
+                        value={this.state.email} 
                         label='email'
                         required 
                     />
                     <FormInput  
                         name='password' 
                         type='password' 
-                        value={this.state.password} 
                         handleChange={this.handleChange}
+                        value={this.state.password} 
                         label='password'
                         required 
                     />
