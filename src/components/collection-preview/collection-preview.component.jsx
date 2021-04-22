770bae42092828b2c4614cd78b996ab88e569473
Simon Catalin <simoncatalin@Simons-MacBook-Air.local>
2021-04-22 14:55:51 +0300

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -11,8 +11,8 @@ const CollectionPreview = ({ title, items }) => (
             {items
                 .filter((item, idx) => idx < 4)
                 .map(item  => (
-                <CollectionItem key={item.id} item={item} />
-            ))}
+                    <CollectionItem key={item.id} item={item} />
+                ))}
         </div>
     </div>
 );
--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,20 +4,20 @@ import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import './collections-overview.style.scss';
 
 const CollectionsOverview = ({ collections }) => (
     <div className='collections-overview'>
-        {collections.map(({id, ...otherCollectionProps}) => (
+        {collections.map(({ id, ...otherCollectionProps }) => (
             <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
     </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
