676f7f79711472283a5dd2f65a8e1c6c15e7d29c
Simon Catalin <simoncatalin@Simons-MacBook-Air.local>
2021-10-13 16:54:50 +0300

    added root-saga

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,23 @@
+import { takeLatest, call, put } from 'redux-saga/effects';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions';
+import ShopActionsTypes from './shop.types';
+
+
+export function* fetchCollectionsAsync() {
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+        yield put(fetchCollectionsSuccess(collectionsMap));
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error.message));
+    }
+}
+
+export function* fetchCollectionsStart() {
+    yield takeLatest(
+        ShopActionsTypes.FETCH_COLLECTIONS_START, 
+        fetchCollectionsAsync    
+    );
+}
